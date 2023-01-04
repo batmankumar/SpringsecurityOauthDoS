1abf90004bdfa5ba78b3928b6100ff633756ad2b
batkumar <77351174+batmankumar@users.noreply.github.com>
2023-01-03 21:01:11 -0600

    Add files via upload

---
+++ SpringSecurityoauth2/src/main/java/com/example/SpringSecurityoauth2/SpringSecurityoauth2Application.java
@@ -0,0 +1,13 @@
+package com.example.SpringSecurityoauth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityoauth2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityoauth2Application.class, args);
+	}
+
+}
---
+++ SpringSecurityoauth2/src/test/java/com/example/SpringSecurityoauth2/SpringSecurityoauth2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringSecurityoauth2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityoauth2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
